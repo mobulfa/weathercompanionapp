29da0d3b6adb7a2f78023548a6dc92fd9b95d111
manuelito <manuelito.bulfa@gmail.com>
2023-06-02 16:34:55 +0800

    add script

--- script.js
+++ script.js
@@ -1,20 +1,98 @@
 const currentTimeT = () => {
                 //Time Script
-                var today = new Date();
-                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                var time = new Date();
+                //var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                
                 const currentTime1 = document.getElementById('currentTime');
-                currentTime1.textContent = time;
+                currentTime1.textContent = time.toLocaleString(('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }));
 }
 currentTimeT();
 
 const apiKey = "r1mSiqCerHW8c04GI5X5UU85oFTHz792";
-const apiUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?q=Bansalan";
+const apiUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?q=";
+
 
 const form = document.querySelector('#searchCity');
 const searchCity = form.elements.q.value;
 
+const config2 = {params: {apikey: apiKey}, headers:{}};
+
+form.addEventListener('submit', async function(e) {
+	e.preventDefault();
+	 const searchTerm = form.elements.q.value;
+    const config = {params: {q: searchTerm, apikey: apiKey}, headers:{}};
+    const res = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?`, config);
+    
+    currentWeather(res.data);
+
+    
+    form.elements.q.value = '';		
+})
+
+const currentWeather = async (data) => {
+    for(let current of data){
+        console.log(current);
+ 
+            const localizedName = document.getElementById('localizedName');
+            const countryCode = document.getElementById('countryCode');
+            localizedName.textContent = current.LocalizedName;
+        //   countryCode.textContent = current.EnglishName;
+            const locationKey = current.Key;
+
+        if(current.Key){
+           
+                const checkCurrent = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?`, config2);
+                // console.log(checkCurrent).data;
+                dailyWeather(checkCurrent.data);
+                
+   
+        }
+      }
+}
+
+const dailyWeather = async (datacurrent) => {
+            const img = document.getElementById('wIcon');
+            const currentTemp = document.getElementById('currentTemp');
+            const status = document.getElementById('status');
+
+            for(data of datacurrent){
+                
+                const iconW = data.WeatherText;
+
+                if(iconW === "Cloudy"){
+                    img.setAttribute('src', './images/clouds2.png'); 
+                }
+
+                const tempC = data.Temperature.Metric.Value;
+                //const celciusMin = ((minTemp - 32) * 5 / 9);
+                currentTemp.textContent = Math.round(tempC);
+
+                console.log(data);
+                status.textContent = data.WeatherText;  
+                // console.log(iconW);
+            }
+
+         
+                
+               
+
+             
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 // async function checkWeather() {
-//     const response = await fetch(apiUrl  + `&apikey=${apiKey}`);
+//     const response = await axios.get(apiUrl + `${searchCity}`  + `&apikey=${apiKey}`);
 //     var data = response.json();
 
 //     document.getElementById('localizedName').innerHTML = data.LocalizedName;
@@ -23,7 +101,7 @@ const searchCity = form.elements.q.value;
  
 //     // document.getElementById('countryID').innerHTML = data.Country.ID;
 
-//    console.log(data.LocalizedName);
+//    console.log(data);
 // }
 
 // checkWeather();
@@ -82,30 +160,4 @@ const searchCity = form.elements.q.value;
 
 
 
-// form.addEventListener('submit', async function(e) {
-// 	e.preventDefault();
-// 	const searchTerm = form.elements.q.value;
-//     const response = await fetch(apiUrl + searchTerm + `&apikey=${apiKey}`);
-//     var data = response.json();
 
-//     //currentWeather(data);
-//     console.log(data);
-//     form.elements.q.value = '';		
-// })
-
-// const currentWeather = async (data) => {
-//     for(let current of data){
-//         console.log(current);
-//         // if (current.EnglishName) {
-            
-
-
-//         //     //current Weather script
-//         //     // const cure = document.getElementById('currentWeather');
-//         //     const localizedName = document.getElementById('localizedName');
-//         //     const countryCode = document.getElementById('countryCode');
-//         //     localizedName.textContent = current.LocalizedName;
-
-//         // }
-//     }
-// }
