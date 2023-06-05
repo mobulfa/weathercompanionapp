d1c0a7ea8bae896d6e354779f168606bde386c6b
manuelito <manuelito.bulfa@gmail.com>
2023-06-05 14:58:22 +0800

    added script

--- script.js
+++ script.js
@@ -17,13 +17,22 @@ const searchCity = form.elements.q.value;
 
 const config2 = {params: {apikey: apiKey}, headers:{}};
 
+
+
 form.addEventListener('submit', async function(e) {
 	e.preventDefault();
 	 const searchTerm = form.elements.q.value;
     const config = {params: {q: searchTerm, apikey: apiKey}, headers:{}};
     const res = await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?`, config);
+
+    
+
+
     
     checkCity(res.data);
+
+    dailyForcast(res.data);
+    
 
     
     form.elements.q.value = '';		
@@ -31,12 +40,13 @@ form.addEventListener('submit', async function(e) {
 
 const checkCity = async (data) => {
     for(let current of data){
-        console.log(current);
+        //  console.log(current.AdministrativeArea.CountryID);
  
             const localizedName = document.getElementById('localizedName');
             const countryCode = document.getElementById('countryCode');
-            localizedName.textContent = current.LocalizedName;
-        //   countryCode.textContent = current.EnglishName;
+            const countryID = current.AdministrativeArea.CountryID;
+            localizedName.textContent = current.LocalizedName + ', '+ countryID;
+        //    countryCode.textContent = countryID;
             const locationKey = current.Key;
 
         if(current.Key){
@@ -71,12 +81,86 @@ const currentWeather = async (datacurrent) => {
                 //const celciusMin = ((minTemp - 32) * 5 / 9);
                 currentTemp.textContent = Math.round(tempC);
 
-                console.log(data);
+                // console.log(data);
                 status.textContent = data.WeatherText;  
                 // console.log(iconW);
+
+           
+                //  console.log(checkday.data);
+                // dailyForcast(checkday.data);
             }          
 }
 
-const dailyForcast = async () => {
+const dailyForcast = async (data) => {
+    for (let datas of data){
+
+         
+        const locationKey = datas.Key;
+        const checkday = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?`, config2);
+        console.log(checkday.data.DailyForecasts);
+          const dayCheck = checkday.data.DailyForecasts;
+
+          for (let days of dayCheck){
+
+            console.log(days.Temperature);
+
+            const dayTemp = document.getElementById('dayTemp');
+            const nightTemp = document.getElementById('nightTemp');
+          
+            const imgDay = document.getElementById('wIconday');
+          
+            const imgNight = document.getElementById('wIconnight');
+
+            const statusDay = document.getElementById('statusDay');
+  
+            const statusNight = document.getElementById('statusNight');
+
+       
+    
+            const minTemp = days.Temperature.Minimum.Value;
         
+            const maxTemp = days.Temperature.Maximum.Value;
+  
+            const celciusMin = ((minTemp - 32) * 5 / 9);
+  
+            const celciusMax = ((maxTemp - 32) * 5 / 9);
+  
+            const iconWday = days.Day.IconPhrase;
+  
+            if(iconWday === "Cloudy"){
+              imgDay.setAttribute('src', './images/clouds2.png'); 
+            }
+            if(iconWday === "Rain shower"){
+              imgDay.setAttribute('src', './images/Rainy.png')
+            }
+            if(iconWday === "Thunderstorms"){
+              imgDay.setAttribute('src', './images/thunder.png')
+            }
+            
+  
+            dayTemp.textContent = Math.round(celciusMin) 
+  
+            nightTemp.textContent = Math.round(celciusMax) 
+  
+  
+            const iconWnight = days.Night.IconPhrase;
+  
+            if(iconWnight === "Cloudy"){
+              imgNight.setAttribute('src', './images/clouds2.png'); 
+            }
+            if(iconWnight === "Rain shower"){
+              imgNight.setAttribute('src', './images/Rainy.png')
+            }
+            if(iconWnight === "Thunderstorms"){
+              imgNight.setAttribute('src', './images/thunder.png')
+            }
+            
+            statusDay.textContent = days.Day.IconPhrase;
+            statusNight.textContent = days.Night.IconPhrase;
+          }
+
+    }
+
+
+   
 }
