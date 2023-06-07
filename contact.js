6ed079aaecf433a7cce10f6ca6e18fea8371c9bb
Renier <renier.esber@gmail.com>
2023-06-07 18:57:25 +0800

    for todays checkpoint

---
+++ contact.js
@@ -0,0 +1,21 @@
+const navFunc = document.querySelector('nav'); 
+const uList = document.querySelector('ul');
+
+
+
+
+
+
+
+
+// navFunc.addEventListener('click', event =>{
+// 	event.preventDefault();
+// 	const task = document.querySelector('#new-task').value.trim();
+// 	// if (task === '') {
+		
+// 		return;
+// 	} else {
+		
+
+// 	}
+// });
