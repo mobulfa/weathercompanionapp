69ce267330de11824629230fbe10a11e71e41a5a
manuelito <manuelito.bulfa@gmail.com>
2023-06-08 12:56:31 +0800

    edit about index and script

--- script.js
+++ script.js
@@ -12,8 +12,127 @@ const apiKey = "r1mSiqCerHW8c04GI5X5UU85oFTHz792";
 const apiUrl = "https://dataservice.accuweather.com/locations/v1/cities/search?q=";
 const apiUrlgeoPosition = "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=";
 
+document.body.onload = function (){
+  userLocation();
+};
 
 
+// ----------START of Functions for GeoPosition Search
+const btnLocation = document.getElementById('btnlocation');
+
+
+const userLocation = () => {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(currentLocation);
+  } else {
+    showLocation.innerHTML = "Your browser does not support this feature.";
+  }
+}
+
+const currentLocation = async (lat) => {
+  const currentPositionlat = lat.coords.latitude;
+  const currentPositionlong = lat.coords.longitude;
+  const locationResult = await axios.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?q=${currentPositionlat},${currentPositionlong}`, config2);
+
+   getCurrentLocation(locationResult.data);
+
+}
+
+const getCurrentLocation = async (data) => {
+  const localizedName = document.getElementById('localizedName');
+  const cityName = data.ParentCity.LocalizedName;
+  const countryID = data.AdministrativeArea.CountryID;
+  const locationKey = data.Key;
+  localizedName.textContent = data.LocalizedName + ', '+ cityName + ', ' + countryID;
+
+  if(data.Key){
+           
+    const checkCurrent = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?`, config2);
+  
+    currentWeather(checkCurrent.data);
+}
+dailyForcastGeo(data);
+}
+const dailyForcastGeo = async (data) => {
+ 
+
+       
+      const locationKey = data.Key;
+      const checkday = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?`, config2);
+   //   console.log(checkday.data.DailyForecasts);
+        const dayCheck = checkday.data.DailyForecasts;
+
+        for (let days of dayCheck){
+
+         // console.log(days.Temperature);
+
+          const dayTemp = document.getElementById('dayTemp');
+          const nightTemp = document.getElementById('nightTemp');
+        
+          const imgDay = document.getElementById('wIconday');
+        
+          const imgNight = document.getElementById('wIconnight');
+
+          const statusDay = document.getElementById('statusDay');
+
+          const statusNight = document.getElementById('statusNight');
+
+     
+  
+          const minTemp = days.Temperature.Minimum.Value;
+      
+          const maxTemp = days.Temperature.Maximum.Value;
+
+          const celciusMin = ((minTemp - 32) * 5 / 9);
+
+          const celciusMax = ((maxTemp - 32) * 5 / 9);
+
+          const iconWday = days.Day.IconPhrase;
+
+          if(iconWday === "Cloudy"){
+            imgDay.setAttribute('src', './images/clouds2.png'); 
+          }
+          if(iconWday === "Rain shower"){
+            imgDay.setAttribute('src', './images/Rainy.png')
+          }
+          if(iconWday === "Thunderstorms"){
+            imgDay.setAttribute('src', './images/thunder.png')
+          }
+          if(iconWday === "Showers"){
+              imgDay.setAttribute('src', './images/rain.png')
+            }
+  
+          
+
+          dayTemp.textContent = Math.round(celciusMin) 
+
+          nightTemp.textContent = Math.round(celciusMax) 
+
+
+          const iconWnight = days.Night.IconPhrase;
+
+          if(iconWnight === "Cloudy"){
+            imgNight.setAttribute('src', './images/clouds2.png'); 
+          }
+          if(iconWnight === "Rain shower"){
+            imgNight.setAttribute('src', './images/Rainy.png')
+          }
+          if(iconWnight === "Thunderstorms"){
+            imgNight.setAttribute('src', './images/thunder.png')
+          }
+          if(iconWnight === "Showers"){
+              imgNight.setAttribute('src', './images/Rainy.png')
+            }
+          
+          statusDay.textContent = days.Day.IconPhrase;
+          statusNight.textContent = days.Night.IconPhrase;
+        }
+    
+}
+
+// ----------END OF Functions for GeoPosition Search
+
+// ----------START OF Functions for City Search
 
 const form = document.querySelector('#searchCity');
 const searchCity = form.elements.q.value;
@@ -31,27 +150,24 @@ form.addEventListener('submit', async function(e) {
     checkCity(res.data);
 
     dailyForcast(res.data);
-    
 
-    
     form.elements.q.value = '';		
 })
 
 const checkCity = async (data) => {
     for(let current of data){
-        //  console.log(current.AdministrativeArea.CountryID);
- 
+      
             const localizedName = document.getElementById('localizedName');
             const countryCode = document.getElementById('countryCode');
             const countryID = current.AdministrativeArea.CountryID;
             localizedName.textContent = current.LocalizedName + ', '+ countryID;
-        //    countryCode.textContent = countryID;
+      
             const locationKey = current.Key;
 
         if(current.Key){
            
                 const checkCurrent = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?`, config2);
-                // console.log(checkCurrent).data;
+              
                 currentWeather(checkCurrent.data);
         }
       }
@@ -84,13 +200,8 @@ const currentWeather = async (datacurrent) => {
                 //const celciusMin = ((minTemp - 32) * 5 / 9);
                 currentTemp.textContent = Math.round(tempC);
 
-                // console.log(data);
-                status.textContent = data.WeatherText;  
-                // console.log(iconW);
-
-           
-                //  console.log(checkday.data);
-                // dailyForcast(checkday.data);
+               status.textContent = data.WeatherText;  
+ 
             }          
 }
 
@@ -100,12 +211,12 @@ const dailyForcast = async (data) => {
          
         const locationKey = datas.Key;
         const checkday = await axios.get(`https://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}?`, config2);
-        console.log(checkday.data.DailyForecasts);
+    //    console.log(checkday.data.DailyForecasts);
           const dayCheck = checkday.data.DailyForecasts;
 
           for (let days of dayCheck){
 
-            console.log(days.Temperature);
+   //         console.log(days.Temperature);
 
             const dayTemp = document.getElementById('dayTemp');
             const nightTemp = document.getElementById('nightTemp');
@@ -147,7 +258,7 @@ const dailyForcast = async (data) => {
   
             dayTemp.textContent = Math.round(celciusMin) 
   
-            nightTemp.textContent = Math.round(celciusMax) 
+            nightTemp.textContent = Math.round(celciusMin) 
   
   
             const iconWnight = days.Night.IconPhrase;
@@ -168,6 +279,6 @@ const dailyForcast = async (data) => {
             statusDay.textContent = days.Day.IconPhrase;
             statusNight.textContent = days.Night.IconPhrase;
           }
-
     }   
 }
+// ----------END OF Functions for City Search
